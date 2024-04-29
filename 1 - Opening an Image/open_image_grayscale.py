c49e1f2827028a88a7756672fd38844824405105
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-04-29 14:35:44 +0800

    added a script to open an image in grayscale and save as png

---
+++ 1 - Opening an Image/open_image_grayscale.py
@@ -0,0 +1,19 @@
+# import opencv library
+import cv2
+
+# open an image
+img = cv2.imread("test_image.jpg")
+
+# convert from BGR to grayscale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# show image in a named window
+cv2.imshow("Original", img)
+# show grayscale in a new named window
+cv2.imshow("Grayscale", img_gray)
+
+# create a new image for the grayscale image in png
+cv2.imwrite("test_image_gray.png", img_gray)
+
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
