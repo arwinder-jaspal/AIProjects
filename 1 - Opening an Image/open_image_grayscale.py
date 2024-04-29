6b071d881614aa9d28e0d342165afa983781dc45
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-04-29 14:41:18 +0800

    add prints to get img shape and size

--- 1 - Opening an Image/open_image_grayscale.py
+++ 1 - Opening an Image/open_image_grayscale.py
@@ -17,3 +17,8 @@ cv2.imwrite("test_image_gray.png", img_gray)
 
 cv2.waitKey(5000)
 cv2.destroyAllWindows()
+
+# check img shape
+print(img.shape)  # returns image height, width and depth as a tuple
+
+print(img.size)  # returns total number of element in the image array (ie height * width * depth
