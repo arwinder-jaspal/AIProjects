6a3c3e67f2d91f1d07846d0a7ac83c6e78529e21
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-05-08 09:04:20 +0800

    resize image and apply gaussian blur

--- 2 - Image Resizing/moving_object_detection.py
+++ 2 - Image Resizing/moving_object_detection.py
@@ -1,14 +1,19 @@
 # import necessary library
 import cv2
+import imutils
 
 # open camera capture
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-
+    # resize frame
+    frame = imutils.resize(1000)
     # convert image to grayscale
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # apply gaussian blur to smoothen image
+    gaussian_blur_img = cv2.GaussianBlur(frame_gray, (21, 21),0)
 
     # display camera feed
     cv2.imshow("Live Feed", frame)
