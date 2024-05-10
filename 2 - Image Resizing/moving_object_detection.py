e82d39aa27b0bf2a60e1cec3ee242f64a7da21b7
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-05-11 01:19:15 +0800

    added image thresholding

--- 2 - Image Resizing/moving_object_detection.py
+++ 2 - Image Resizing/moving_object_detection.py
@@ -22,8 +22,23 @@ while True:
         first_frame = gaussian_blur_img
         continue
 
+    # calculate absolute difference between first frame and current frame
+    # this will show only the difference in the image
+    image_diff = cv2.absdiff(first_frame, gaussian_blur_img)
+
+    # apply binary thresholding to the difference
+    # we apply thresholding on the different to highlight the difference so that it easier to draw a bounding box
+    _, threshold_img = cv2.threshold(image_diff, 180, 255, cv2.THRESH_BINARY)
+
+    # apply dilation to fill up the images after binary threshold
+    dilated_img = cv2.dilate(threshold_img, None, iterations=2)
+
+    # find contours of the dilated image
+    contour = cv2.findContours(dilated_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
     # display camera feed
-    cv2.imshow("Live Feed", frame)
+        cv2.imshow("Live Feed", frame)
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
