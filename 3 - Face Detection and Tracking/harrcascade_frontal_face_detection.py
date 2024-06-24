4f72c447917629953491de15feb0bfa2349f6257
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-06-24 10:47:48 +0800

    draw bounding box on the detected face

--- 3 - Face Detection and Tracking/harrcascade_frontal_face_detection.py
+++ 3 - Face Detection and Tracking/harrcascade_frontal_face_detection.py
@@ -17,6 +17,12 @@ while True:
     # getting face coordinate
     face = haar_cascade.detectMultiScale(gray_img, scaleFactor=1.3, minNeighbors=4)
 
+    for (x, y, w, h) in face:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=2)
+    cv2.imshow("FaceDetection", frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
