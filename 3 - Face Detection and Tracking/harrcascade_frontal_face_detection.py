1f1bad1ce8c49b0a5b026011f2e16e3435794331
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-06-24 10:32:51 +0800

    read from camera and get face coordinate

--- 3 - Face Detection and Tracking/harrcascade_frontal_face_detection.py
+++ 3 - Face Detection and Tracking/harrcascade_frontal_face_detection.py
@@ -8,7 +8,15 @@ haar_cascade = cv2.CascadeClassifier(algo)
 cap = cv2. VideoCapture(0)
 
 while True:
-    break
+    # read image from caemra
+    ret, frame = cap.read()
+
+    # convert image to grayscale
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # getting face coordinate
+    face = haar_cascade.detectMultiScale(gray_img, scaleFactor=1.3, minNeighbors=4)
+
 
 cap.release()
 cv2.destroyAllWindows()
