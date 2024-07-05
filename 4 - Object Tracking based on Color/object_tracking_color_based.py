c7f76e2696a79b144105abe214412ff9bca92b84
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-07-05 11:59:37 +0800

    fixed hsv range to be a tuple instead of a list

--- 4 - Object Tracking based on Color/object_tracking_color_based.py
+++ 4 - Object Tracking based on Color/object_tracking_color_based.py
@@ -2,8 +2,8 @@ import cv2
 import imutils
 
 # define hsv lower and upper bound for the color of your choice
-hsv_lower = [0, 129, 155]
-hsv_upper = [30, 255, 240]
+hsv_lower = (0, 129, 155)
+hsv_upper = (30, 255, 240)
 
 # initialize video capture
 cap = cv2.VideoCapture(6)
@@ -21,5 +21,3 @@ while True:
 
     # convert color to HSV
     hsv_image = cv2.cvtColor(gaussian_blur_img, cv2.COLOR_BGR2HSV)
-
-
