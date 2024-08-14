7605835285e1d4bd4f7844aee2fe021ad5e5772f
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-08-14 19:10:12 +0800

    crop the image in the bounding box and write it to a file

--- 5 - Face Recognition/create_dataset_from_camera.py
+++ 5 - Face Recognition/create_dataset_from_camera.py
@@ -3,6 +3,17 @@ import cv2, os
 # specify xml for haarcascade
 haar_file = 'haarcascade_frontalface_default.xml'
 
+# create the path for dataset
+dataset_dir = 'datasets'
+sub_data_dir = 'arwinder'
+
+dataset_dir_path = os.path.join(dataset_dir, sub_data_dir)
+if not os.path.isdir(dataset_dir_path):
+    os.mkdir(dataset_dir_path)
+
+# Set the dimensional for the output image
+(width, height) = (130, 100)
+
 # load haar cascade classifier algorithm
 face_cascade = cv2.CascadeClassifier(haar_file)
 
@@ -21,6 +32,13 @@ while num_captured < 50:
     # draw a bounding box around the detected face
     for (x, y, w, h) in face_coordinates:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0 ,255), 2)
+        # slice the image to have the content of the bounding box only
+        face = frame_grayscale[y:y+h, x:x+w]
+        # resize image to desired size
+        face_resized = cv2.resize(face, (width,height))
+        # write resized face to a file
+        cv2.imwrite("{}/{}.png".format(dataset_dir_path, num_captured), face_resized)
+        num_captured += 1
     # display each frame
     cv2.imshow("Face", frame)
 
@@ -30,5 +48,3 @@ while num_captured < 50:
 
 cap.release()
 cv2.destroyAllWindows()
-
-
