eceb1f88e9a6c187fc99dd0c43d16f9cc961eb47
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-08-21 00:28:41 +0800

    added facial detection

--- 5 - Face Recognition/face_recognition.py
+++ 5 - Face Recognition/face_recognition.py
@@ -28,4 +28,24 @@ for (subdirs, dirs, files) in os.walk(dataset_dir):
 model = cv2.face.FisherFaceRecognizer.create()
 model.train(images, labels)
 
+# load haar cascade classifier algorithm
+face_cascade = cv2.CascadeClassifier(haar_file)
+
+# take in the frames from the camera live feed and compare with the datasets
+cap = cv2.VideoCapture(0)
+while True:
+    _, frame = cap.read()
+    frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # apply haar cascade frontal face algo here
+    faces = face_cascade.detectMultiScale(frame_grayscale, 1.3, 5)
+
+    for (x ,y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.imshow("Face", frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
