66026bdf19a6d4fdc3d32f6a8c56aa947b7c30a4
arwinder-jaspal <arwinderjaspal@gmail.com>
2024-09-08 11:11:47 +0800

    added object detection using caffe model

---
+++ 8 - Object Detection Using Caffe Model/object_detection_using_caffe_model.py
@@ -0,0 +1,80 @@
+import time
+
+import cv2
+import imutils
+import numpy as np
+
+# set classes
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor", "mobile"]
+
+# set colors
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))  # Generate random colors
+
+CONFIDENCE_THRESHOLD = 0.2
+
+# load model
+net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD_deploy.caffemodel")
+
+# initialize the video stream, allow the camera sensor to warmup
+print("[INFO] starting video stream...")
+vs = cv2.VideoCapture(0)
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 1000 pixels
+    ret, frame = vs.read()
+    frame = imutils.resize(frame, width=1000)
+
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    # resize the frame to have a maximum width of 300
+    frame_blob = cv2.resize(frame, (300, 300))
+    blob = cv2.dnn.blobFromImage(frame_blob, 0.007843, (300, 300), 127.5)
+
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
+
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > CONFIDENCE_THRESHOLD:
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            print("ClassID:", detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # display the prediction
+            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            print("[INFO] {}".format(label))
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                          COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(frame, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+# do a bit of cleanup
+vs.release()
+cv2.destroyAllWindows()
