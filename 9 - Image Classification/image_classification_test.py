5175eb9b62cae71afaf0cbe54abbfcf5ba4f5357
arwinder <arwinderjaspal@gmail.com>
2024-12-30 15:49:27 +0800

    added funtion to classify images

--- 9 - Image Classification/image_classification_test.py
+++ 9 - Image Classification/image_classification_test.py
@@ -12,5 +12,21 @@ model = model_from_json(loaded_model_json)
 model.load_weights("model.weights.h5")
 print("Loaded model from disk")
 
+def classify_image(image_path):
+    img = image.load_img(image_path, target_size=(64, 64))
+    test_img = image.img_to_array(img)
+    test_img = np.expand_dims(test_img, axis=0)
+    result = model.predict(test_img)
 
+    if result[0][0] == 1:
+        prediction = "Thanos"
+    else:
+        prediction = "Joker"
+
+    print("Predicted ", image_path, " as ", prediction, '\n')
+
+path = os.path.dirname(os.path.abspath(__file__)) + "/dataset/test/"
+
+for file in os.listdir(path):
+    classify_image(path + file)
 
